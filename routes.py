8b4e03ae2df3b24587d8559fb7568404e4263a68
bdantr <bogdansapr@gmail.com>
2022-02-21 18:31:19 +0300

    adding about us contacts pahe

--- routes.py
+++ routes.py
@@ -6,3 +6,11 @@ from app import app  # импортирую главный модуль сайт
 def main():  # вызовется функция
     return render_template('index.html')  # вернуть шаблон index.html
 
+@app.routs('/contacts')
+def contacts():
+    return render_template ('contacts.html')
+
+
+@app.routs('/about')
+def contacts():
+    return render_template('about')
